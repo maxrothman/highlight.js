7da5c744c46f86049e33f032f761e7dbeee11a91
Max Rothman <max@collegevine.com>
2019-04-01 17:06:34 -0400

    Add list values

--- src/languages/terraform.js
+++ src/languages/terraform.js
@@ -16,7 +16,48 @@ function(hljs) {
     },
     hljs.C_NUMBER_MODE
   ]
-  
+
+  var STRING = {
+    className: 'string',
+    variants: [
+      {begin: '"', end: '"'},
+      {begin: /<<EOF$/, end: /^EOF$/}  //hack hack
+
+    ],
+    illegal: '\\n',
+    contains: [
+      hljs.BACKSLASH_ESCAPE,
+      {
+        // interpolation
+        className: 'subst',
+        begin: /\$\{/,
+        end: /\}/,
+        contains: [{
+          className: 'symbol',
+          variants: [
+            //functions
+            {begin: /\b[a-zA-Z0-9_-]+\(/},
+            {begin: '\\)'}
+
+          ]
+        }].concat(PRIMITIVES).concat([
+          hljs.QUOTE_STRING_MODE,
+          {
+            //references
+            className: 'type',
+            begin: /[a-zA-Z0-9_-]+(\.([a-zA-Z0-9_-]+|\*))+/
+          }
+        ])
+      }
+    ]
+  }
+  PRIMITIVES.push(STRING)
+
+  var LIST = {
+    begin: '\\[', end: '\\]',
+    contains: PRIMITIVES
+  }
+
   var BLOCK_BODY = [
     hljs.HASH_COMMENT_MODE,
     hljs.C_BLOCK_COMMENT_MODE,
@@ -24,59 +65,22 @@ function(hljs) {
       //key
       className: 'attribute',
       begin: /^\s*\w+/,
-      end: /(?![^=])/,
-      excludeEnd: true
+      end: /(?![^=])/
     },
     {
       //value
       begin: '=\s*.',
-      contains: PRIMITIVES.concat([  // values
-        {
-          //string
-          className: 'string',
-          variants: [
-            {begin: '"', end: '"'},
-            {begin: /<<EOF$/, end: /^EOF$/}  //hack hack
-            
-          ],
-          illegal: '\\n',
-          contains: [
-            hljs.BACKSLASH_ESCAPE,
-            {
-              // interpolation
-              className: 'subst',
-              begin: /\$\{/,
-              end: /\}/,
-              contains: [{
-                className: 'symbol',
-                variants: [
-                  //functions
-                  {begin: /\b[a-zA-Z0-9_-]+\(/},
-                  {begin: '\\)'}
-                  
-                ]
-              }].concat(PRIMITIVES).concat([
-                hljs.QUOTE_STRING_MODE,
-                {
-                  //references
-                  className: 'type',
-                  begin: /[a-zA-Z0-9_-]+(\.([a-zA-Z0-9_-]+|\*))+/
-                }
-              ])
-            }
-          ]
-        }
-      ])
+      contains: PRIMITIVES.concat([LIST]),  // values
     }
   ];
 
   var BLOCK = {
     begin: /^\s*\w+(\s+"\w+")*\s*\{/, returnBegin: true,
-    end: '}', excludeEnd: true,
+    end: /\}/, excludeEnd: true,
     contains: [
       hljs.QUOTE_STRING_MODE,  //block titles
       {
-        begin: '{', end: '}',
+        begin: /\{/, end: '/\}/',
         contains: BLOCK_BODY
       },
       {
